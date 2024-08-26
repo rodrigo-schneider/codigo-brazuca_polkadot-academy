2686d91f2220c6fd6e1c745057aa1d1777c0b5d9
Rodrigo Schneider <55472256+rodrigo-schneider@users.noreply.github.com>
2024-08-26 17:34:43 -0300

    Update 09_Strings.py

--- Exercises - Week 2/09_Strings.py
+++ Exercises - Week 2/09_Strings.py
@@ -1,4 +1,4 @@
-def contarLetra(frase, letra):
+def contar_letra(frase, letra):
     return frase.lower().count(letra.lower())
 
 frase = input("Digite uma frase: ")
@@ -7,5 +7,5 @@ letra = input("Digite a letra que deseja contar: ")
 if len(letra) != 1:
     print("Por favor, digite apenas uma letra.")
 else:
-    quantidade = contarLetra(frase, letra)
+    quantidade = contar_letra(frase, letra)
     print(f"A letra '{letra}' aparece {quantidade} vez(es) na frase.")
