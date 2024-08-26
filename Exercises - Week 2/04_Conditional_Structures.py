49ed5808c262c5033872783502fd6a1b6a4aa33f
Rodrigo Schneider <55472256+rodrigo-schneider@users.noreply.github.com>
2024-08-26 17:09:50 -0300

    Add files via upload

---
+++ Exercises - Week 2/04_Conditional_Structures.py
@@ -0,0 +1,8 @@
+temp = int(input("Informe a temperaturam atual: "))
+
+if temp > 30 :
+    print("A temperatura está quente.")
+elif temp < 15 :
+    print("A temperatura está fria.")
+else :
+    print("A temperatura está agradável.")
