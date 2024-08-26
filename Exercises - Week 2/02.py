6abccc07d317926197555f27d86153f7d83bf1e6
Rodrigo Schneider <55472256+rodrigo-schneider@users.noreply.github.com>
2024-08-26 05:50:50 -0300

    Add files via upload

---
+++ Exercises - Week 2/02.py
@@ -0,0 +1,5 @@
+nomeFilme = input("Qual é o seu filme favorito?")
+
+anoLancamento = input("Qual é o ano de lançamento do seu filme favorito?")
+
+print("O nome do filme é " + nomeFilme + " e seu ano de lançamento é " + anoLancamento)
