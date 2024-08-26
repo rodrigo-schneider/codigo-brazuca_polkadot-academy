4da40566211eb510e6bae40ca65bb81061ea155b
Rodrigo Schneider <55472256+rodrigo-schneider@users.noreply.github.com>
2024-08-26 16:55:23 -0300

    Add files via upload

---
+++ Exercises - Week 2/03.py
@@ -0,0 +1,15 @@
+num1 = int(input("Digite o primeiro número"))
+num2 = int(input("Digite o segundo número"))
+
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
+
+print("A soma dos números é: " + soma)
+
+print("A subtração dos números é: " + subtracao)
+
+print("A multiplicação dos números é: " + multiplicacao)
+
+print("A divisão dos números é: " + divisao)
